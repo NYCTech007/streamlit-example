c815457ff305bed2bb30fae4ee2203bec9bcb1ac
J Robinson <95053418+NYCTech007@users.noreply.github.com>
2023-02-11 11:04:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,64 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+"""
+# Parking data app
+This apps shows the findngs of my dataset, provided by NY Open Data:
+
+"""
 import streamlit as st
+import pandas as pd
+import numpy as np
+st.header('Parking Cap')
+DATA_URL = ('https://raw.githubusercontent.com/NYCTech007/Labs/main/mycsv.csv')
+PCT_locs = ('https://raw.githubusercontent.com/NYCTech007/Labs/main/Pct_locs.csv')
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+#App title
+st.title("Top Ticketing Precincts for 2022")
+@st.experimental_memo
+def load_mapdata(self):
+    pctlocz = pd.read_csv(PCT_locs, nrows=self)
+    return pctlocz 
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# Create a text element and let the reader know the Map is loading.
+pctdata_load_state = st.text('Loading Map...')
+# Load data into the dataframe.
+pctlocz = load_mapdata(1000)
+# Notify the reader that the Map was successfully loaded.
+pctdata_load_state.text('Map data loaded sucessfully!')
 
-    points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+revenue_metric = st.metric(label="NYC's Record Year FY2015", value="59% of annual revenue", delta="$565 million of $957M"), st.text('FY = Fiscal Year'),
+col1, col2, col3 = st.columns(3)
+col1.metric("FY2022 Parking Fines", "OWED", "-$500 Million")
+col3.metric("FY2020 issued tickets", "4,006,011", "Approx $260 Million")
+col2.metric("Tickets Issued", "FY2021", "$138.3 Millioin")
+DATE_COLUMN = 'ts'
+#pctlocs = pd.DataFrame(PCT_locs, columns=["PCT", "longitude", "latitude"])
+NYPD =  pctlocz[["PCT", "longitude", "latitude"]]
+nypct = pctlocz[["PCT"]]
+pctdata = pd.read_csv(DATA_URL)
+pctdata_NYPCT  = pctdata.PRECINCT.unique()
+
+
+
+# Using "with" notation
+with st.sidebar:
+    
+    option = st.selectbox(
+    'Which ticketing precinct interest you?',
+    (pctdata_NYPCT))
+    
+    st.write('You selected Precinct:', option)
+
+
+    
+
+
+
+
+nyc_map = st.map(pctlocz[["latitude", "longitude"]].dropna(),  zoom=None)
+# plot the slider that selects number of tickets
+#ticket_slider = st.slider("Number of tickets", 1, int(pctdata["Number of Tickets"].max()))
+ 
+
